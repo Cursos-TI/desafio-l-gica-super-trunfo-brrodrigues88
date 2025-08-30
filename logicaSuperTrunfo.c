7d038a210183048da7718c5492ec009f3b54b093
brrodrigues88 <brrodrigues6003@gmail.com>
2025-08-30 10:27:11 -0300

    Desafio Aventureiro

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int main() {
     int Carta1, Carta2;
+    int opção;
  
     //Carta1
     char Código1[15] = "A01";
@@ -40,12 +41,30 @@ int main() {
     printf("Área: %.fkm²\n", Área2);
     printf("PIB: %.f Bilhões\n", PIB2);
     printf("Números de Pontos Turísticos: %d\n", Turismo2);
+    printf("\n"); 
 
-    //Comparação
-    if (População1, População2) {
-        printf("\n%s tem uma população menor que %s\n", Cidade1, Cidade2);
-    }
+    //Menus Interativo
+    printf("Escolha uma das opções abaixo para comparar as cartas:\n");
+    printf("1 - População\n");
+    printf("2 - Área\n");
+    printf("Digite a opção desejada: ");
+    scanf("%d", &opção);
 
+    //Comparar Cartas População e Área 
+if (População1 > População2) {
+    printf("# Jacarezinho-Pr Venceu! #\n"); 
+} else if (População1 < População2) {
+    printf("# Campinas-Sp Venceu! #\n");
+} else {
+    printf("# Houve um Empate! #\n");
 
+    if (Área1 > Área2) {
+    printf("# Jacarezinho-Pr Venceu! #\n"); 
+} else if (Área1 < Área2) {
+    printf("# Campinas-Sp Venceu! #\n");
+} else {
+    printf("# Houve um Empate! #\n");
+}
+}
     return 0;
 }
